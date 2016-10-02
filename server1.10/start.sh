563c3a735e67910e4eb9de74c913d02553c1ee76
michael1011 <michael101101@me.com>
2016-10-02 16:21:16 +0200

    removed my server setup

--- server1.10/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Xmx1G -jar spigot-1.10.2.jar
--- server1.10/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm spigot-1.10.2.jar
-
-wget https://michael1011.at/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/spigot-1.10.2.jar
-
-sh ./start.sh
